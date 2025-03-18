6634b03b53711faeaf5bf5a1d568eb2dde582c0d
Ethan <ethanlau88@gmail.com>
2025-03-18 17:27:01 +0000

    movement fix needed

--- GDE demo/Assets/scripts/player_movement.cs
+++ GDE demo/Assets/scripts/player_movement.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class player_movement : MonoBehaviour
+public class JumpKingMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float jumpForce = 10f;
+    public float jumpDirection = 5f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool onGround = true;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Update animator states
+        animator.SetBool("is_idle", onGround);
+        animator.SetBool("is_jumping", !onGround && rb.velocity.y > 0);
+        animator.SetBool("is_falling", !onGround && rb.velocity.y < 0);
+        animator.SetBool("on_ground", onGround);
+
+        if (onGround && (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            Jump(Input.GetKeyDown(KeyCode.LeftArrow) ? -1 : 1);
+        }
+    }
+
+    void Jump(int direction)
+    {
+        rb.velocity = new Vector2(direction * jumpDirection, jumpForce);
+        onGround = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
     }
 }
